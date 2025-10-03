baf8e7f5e6640f5395e9fc3c371b9a64896af245
mamyrbekova <122817142+mamyrbekova@users.noreply.github.com>
2025-10-04 00:55:08 +0500

    swift file

---
+++ assignment1.swift
@@ -0,0 +1,43 @@
+//import UIKit
+
+// Step 1: Declare Variables for Personal Informa&on
+var firstName: String = "Amina"
+var lastName: String = "Mamyrbekova"
+let currentYear: Int = 2025
+var birthYear: Int =  2005
+var age: Int = currentYear - birthYear
+var isStudent: Bool = true
+var height: Double = 1.64
+var hometown: String = "Almaty"
+var studyYear: Int = 4
+
+// Step 2: Create Variables for Your Hobbies and Interests
+var hobby: String = "Tennis"
+var numberOfHobbies: Int = 3
+var favoriteNumber: Int = 8
+var isHobbyCreative: Bool = true
+var specialty: String = "Information Systems"
+var graduationYear: Int = 2026
+var numberOfSiblings: Int = 2
+
+//Bonus
+var 🚀futureGoals: String = "🌟 In the future, I want to find my passion in IT field and travel the world ✈️."
+
+// Step 3: Create a Summary of Your Life Story
+var lifeStory: String = "My name is \(firstName) \(lastName). I am \(age) years old, born in \(birthYear). I am currently a student: \(isStudent). I study \(specialty), and I am in my \(studyYear) year of university. My height is \(height) meters. I live in \(hometown). I have \(numberOfSiblings) siblings. I enjoy \(hobby), which is a creative hobby: \(isHobbyCreative). I have \(numberOfHobbies) hobbies in total, and my favorite number is \(favoriteNumber). I plan to graduate in \(graduationYear). \(🚀futureGoals)"
+
+
+// Step 4: Print Your Life Story
+print(lifeStory)
+
+
+
+
+
+
+
+
+
+
+
+
