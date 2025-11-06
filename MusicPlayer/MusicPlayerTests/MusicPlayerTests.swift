7107085cdd2f49f9195a95c3b732574e914150b6
mamyrbekova <122817142+mamyrbekova@users.noreply.github.com>
2025-11-07 02:53:54 +0500

    Re-add MusicPlayer as normal folder (remove submodule link)

---
+++ MusicPlayer/MusicPlayerTests/MusicPlayerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerTests.swift
+//  MusicPlayerTests
+//
+//  Created by Амина Мамырбекова on 06.11.2025.
+//
+
+import Testing
+@testable import MusicPlayer
+
+struct MusicPlayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
